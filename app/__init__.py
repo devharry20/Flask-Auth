4894fcf3ce414868b95f4275d53abf4e9272de95
Harry <Athonal2019@gmail.com>
2024-12-03 22:29:31 +0000

    Added func docstrings and added google oauth data

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,36 @@
 import os
 
+import requests
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login.login_manager import LoginManager
+from oauthlib.oauth2 import WebApplicationClient
+
 
 db = SQLAlchemy()
 DB_NAME = os.getcwd() + r"\database.db"
 
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+
+def get_google_provider_cfg():
+    """A function to gather data used for Google authentication"""
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
 
 def create_app():
-    print(os.getcwd())
+    """Main function to run Flask app and set config"""
     app = Flask(__name__, template_folder=os.getcwd() + r"\app\pages")
     app.config["SECRET_KEY"] = "abc123"
     app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
     db.init_app(app)
+
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+    global client
+    client = WebApplicationClient(GOOGLE_CLIENT_ID)
 
     from .views import views
     from .auth import auth
