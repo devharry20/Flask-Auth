05558c9975a0e4981164624f5c95643ae3e2d85f
Harry <Athonal2019@gmail.com>
2024-12-03 22:31:54 +0000

    Added docstrings

--- app/views.py
+++ app/views.py
@@ -7,9 +7,11 @@ views = Blueprint("views", __name__)
 
 @views.route("/")
 def index():
+    """A view to render the home page"""
     return render_template("index.html", user=current_user)
 
 @views.route("/account")
 @login_required
 def account():
+    """A protected view to show users account data"""
     return render_template("account.html", user=current_user)
