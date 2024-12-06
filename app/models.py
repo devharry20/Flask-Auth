9b757f88748f5ce0d1a69762e474690a4b9dec21
Harry <Athonal2019@gmail.com>
2024-12-06 22:50:19 +0000

    Added fields for google auth validation

--- app/models.py
+++ app/models.py
@@ -9,4 +9,6 @@ class User(db.Model, UserMixin):
     name = db.Column(db.String(150), nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
+    user_type = db.Column(db.String(20), nullable=False)
+    user_auth = db.Column(db.String(150), nullable=True)
     created = db.Column(db.DateTime(timezone=True), default=func.now())
