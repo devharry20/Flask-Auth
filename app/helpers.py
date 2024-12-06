ad86baa4f3af0a3138a87cc465314c9e8f9c62dd
Harry <Athonal2019@gmail.com>
2024-12-06 22:50:44 +0000

    Added args for extra db model fields

--- app/helpers.py
+++ app/helpers.py
@@ -4,9 +4,9 @@ from .models import User
 from . import db
 
 
-def create_user(email: str, name: str, password: str) -> User:
+def create_user(email: str, name: str, password: str, user_type: str, user_auth: str = None) -> User:
     """A helper function to create a user entry into the database"""
-    new_user = User(email=email, name=name, password=generate_password_hash(password, method="pbkdf2:sha256"))
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method="pbkdf2:sha256"), user_type=user_type, user_auth=user_auth)
     db.session.add(new_user)
     db.session.commit()
 
